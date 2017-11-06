640b685e483af33fa201565a72fd2b7d4d9fa2a9
GuillaumeDgr <dangerguillaume@gmail.com>
2017-11-06 10:26:35 +0100

    GoogleMaps activity added

--- app/src/main/java/fr/wcs/gitworkflow/MainActivity.java
+++ app/src/main/java/fr/wcs/gitworkflow/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.wcs.gitworkflow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -12,6 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Link to MapsActivity
+        Button buttonSend = (Button) findViewById(R.id.buttonSend);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+            }
+        });
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
